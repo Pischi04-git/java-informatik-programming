a9240fdebb076be2d646550bc5187cd5f7ca2b31
Pischi04-git <github@fabianhengesbach.de>
2025-12-12 09:06:15 +0100

    Created Aufgabe2.java

---
+++ 12.12.2025/Aufgabe2.java
@@ -0,0 +1,15 @@
+// Dieses Programm demonstriert verschiedene Rechenoperationen.
+
+public class Aufgabe2 {
+    public static void main(String[] args) {
+        
+        // Gibt das Ergebnis der Berechnung aus
+
+        System.out.println(19+5); // Addiert zwei Zahlen
+        System.out.println(19-5); // Subtrahiert die zweite Zahl von der ersten
+        System.out.println(19*5); // Multipliziert die zwei Zahlen
+        System.out.println(19/5); // Dividiert die beiden Zahlen und rundet ab
+        System.out.println(19%5); // Berechnet den Rest / Modulo
+        System.out.println(19./5.); // Dividiert die beiden Zahlen und gibt das Ergebnis als float / Dezimalzahl aus
+    }
+}
