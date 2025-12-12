8eec3c66e9797ba0fd8fc8814a90afc5d3a1e016
Pischi04-git <github@fabianhengesbach.de>
2025-12-12 09:15:29 +0100

    Aufgabe2.java modified

--- 12.12.2025/Aufgabe2.java
+++ 12.12.2025/Aufgabe2.java
@@ -8,7 +8,7 @@ public class Aufgabe2 {
         System.out.println(19+5); // Addiert zwei Zahlen
         System.out.println(19-5); // Subtrahiert die zweite Zahl von der ersten
         System.out.println(19*5); // Multipliziert die zwei Zahlen
-        System.out.println(19/5); // Dividiert die beiden Zahlen und rundet ab
+        System.out.println(19/5); // Dividiert die beiden Zahlen und rundet ab (Ganzzahlige Division)
         System.out.println(19%5); // Berechnet den Rest / Modulo
         System.out.println(19./5.); // Dividiert die beiden Zahlen und gibt das Ergebnis als float / Dezimalzahl aus
     }
