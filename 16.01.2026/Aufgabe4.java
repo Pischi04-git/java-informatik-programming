3ea5fa13a27b1255fbcbbc64fec3b1b0377de577
Pischi04-git <github@fabianhengesbach.de>
2026-01-16 08:47:45 +0100

    updated var klassenkasse name

--- 16.01.2026/Aufgabe4.java
+++ 16.01.2026/Aufgabe4.java
@@ -1,15 +1,15 @@
 public class Aufgabe4 {
     public static void main(String[] args) {
-        int klassenkasse = 0;
+        int Klassenkasse = 0;
         
-        klassenkasse = klassenkasse + 27 * 5; // 27 Schüler zahlen 5 Euro
-        klassenkasse = klassenkasse + 3 * 6; // 3 Schüler zahlen 6 Euro
+        Klassenkasse = Klassenkasse + 27 * 5; // 27 Schüler zahlen 5 Euro
+        Klassenkasse = Klassenkasse + 3 * 6; // 3 Schüler zahlen 6 Euro
 
-        klassenkasse = klassenkasse - 30; // Bastelmaterial
-        klassenkasse = klassenkasse - 60; // Schwimmbadkosten
+        Klassenkasse = Klassenkasse - 30; // Bastelmaterial
+        Klassenkasse = Klassenkasse - 60; // Schwimmbadkosten
 
-        System.out.println("Jeder Schüler bekommt " + klassenkasse/30 + " Euro zurück");
-        System.out.println("Rest in der Kasse: " + klassenkasse%30 + " Euro");
-        System.out.println("Genau Auszahlung pro Schüler: " + klassenkasse / 30. + " Euro");
+        System.out.println("Jeder Schüler bekommt " + Klassenkasse/30 + " Euro zurück");
+        System.out.println("Rest in der Kasse: " + Klassenkasse%30 + " Euro");
+        System.out.println("Genau Auszahlung pro Schüler: " + Klassenkasse / 30. + " Euro");
     }
 }
