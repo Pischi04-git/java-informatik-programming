5670f0746c72e839e9de7d824c7ecbc92347d571
Pischi04-git <github@fabianhengesbach.de>
2026-01-16 08:48:30 +0100

    Updated Comments

--- 16.01.2026/Aufgabe4.java
+++ 16.01.2026/Aufgabe4.java
@@ -2,11 +2,11 @@ public class Aufgabe4 {
     public static void main(String[] args) {
         int Klassenkasse = 0;
         
-        Klassenkasse = Klassenkasse + 27 * 5; // 27 Schüler zahlen 5 Euro
-        Klassenkasse = Klassenkasse + 3 * 6; // 3 Schüler zahlen 6 Euro
+        Klassenkasse = Klassenkasse + 27 * 5; // 27 Schüler zahlen jeweils 5 Euro
+        Klassenkasse = Klassenkasse + 3 * 6; // 3 Schüler zahlen jeweils 6 Euro
 
-        Klassenkasse = Klassenkasse - 30; // Bastelmaterial
-        Klassenkasse = Klassenkasse - 60; // Schwimmbadkosten
+        Klassenkasse = Klassenkasse - 30; // Kosten für Bastelmaterial
+        Klassenkasse = Klassenkasse - 60; // Kosten für den Schwimmbadbesuch
 
         System.out.println("Jeder Schüler bekommt " + Klassenkasse/30 + " Euro zurück");
         System.out.println("Rest in der Kasse: " + Klassenkasse%30 + " Euro");
