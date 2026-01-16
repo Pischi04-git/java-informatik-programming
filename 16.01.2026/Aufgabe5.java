80ffc8a3f8116285b8e29c75ac6d9ee9fd4af0a0
Pischi04-git <github@fabianhengesbach.de>
2026-01-16 08:56:56 +0100

    Created Aufgabe5.java

---
+++ 16.01.2026/Aufgabe5.java
@@ -0,0 +1,8 @@
+public class Aufgabe5 {
+    public static void main(String[] args) {
+        int ersteZahl = 1; // Deklaration und Initialisierung der ersten Zahl
+        int zweiteZahl = 2; // Deklaration und Initialisierung der zweiten Zahl
+        boolean vergleich = ersteZahl <= zweiteZahl; // Vergleich der beiden Zahlen, falls die erste Zahl kleiner oder gleich der zweiten Zahl ist wird die Variable Vergleich auf Wahr gesetzt
+        System.out.println(vergleich); // Ausgabe des Vergleichsergebnisses
+    }
+}
