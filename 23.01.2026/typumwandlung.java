73baba8033ff07c2fe4b1be0dea3229c415d60bb
Pischi04-git <github@fabianhengesbach.de>
2026-01-23 08:19:43 +0100

    Added TypUmwandlung.java

---
+++ 23.01.2026/typumwandlung.java
@@ -0,0 +1,13 @@
+public class typumwandlung {
+    public static void main(String[] args) {
+        int zahl = 10;
+        System.out.println(zahl);
+        double zahl2 = zahl;
+        System.out.println(zahl2);
+
+
+        double zahl3 = 7.9;
+        int zahl4 = (int) zahl3;
+        System.out.println(zahl4);
+    }
+}
