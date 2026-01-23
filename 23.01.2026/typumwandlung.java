8b98b46fe8603d96cbe4988e2ffda1ae12783519
Pischi04-git <github@fabianhengesbach.de>
2026-01-23 08:22:38 +0100

    edited tympumwandlug

--- 23.01.2026/typumwandlung.java
+++ 23.01.2026/typumwandlung.java
@@ -1,11 +1,13 @@
 public class typumwandlung {
     public static void main(String[] args) {
+        
+        // Implizite Typumwandlung
         int zahl = 10;
         System.out.println(zahl);
         double zahl2 = zahl;
         System.out.println(zahl2);
 
-
+        // Explizite Typumwandlung
         double zahl3 = 7.9;
         int zahl4 = (int) zahl3;
         System.out.println(zahl4);
