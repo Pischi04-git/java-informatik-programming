b36876501e9d6c36d452d94fb0a15cfa17c179d9
Pischi04-git <github@fabianhengesbach.de>
2026-01-23 07:56:23 +0100

    Updated Var Name

--- 23.01.2026/aufgabe6.java
+++ 23.01.2026/aufgabe6.java
@@ -3,16 +3,16 @@ public class aufgabe6 {
         int[] x = {1, 3, 7, 10};
         int[] y = {8, 24, 56, 80};
 
-        int[] PropFaktoen = new int[x.length];
+        int[] PropFaktoren = new int[x.length];
         for (int i = 0; i < x.length; i++) {
-            PropFaktoen[i] = y[i] / x[i];
+            PropFaktoren[i] = y[i] / x[i];
         }
         
-        int vorherigerFaktor = PropFaktoen[0];
+        int vorherigerFaktor = PropFaktoren[0];
         boolean propZusammenhang = true;
 
-        for (int faktor = 0; faktor < PropFaktoen.length; faktor++) {
-            if (PropFaktoen[faktor] != vorherigerFaktor) {
+        for (int faktor = 0; faktor < PropFaktoren.length; faktor++) {
+            if (PropFaktoren[faktor] != vorherigerFaktor) {
                 propZusammenhang = false;
                 break;
             }
