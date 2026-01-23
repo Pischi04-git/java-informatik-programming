5b3800fda51a659729ebe71a0850591c2aa31a39
Pischi04-git <github@fabianhengesbach.de>
2026-01-23 08:52:06 +0100

    sd

---
+++ 23.01.2026/aufgabe8.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class aufgabe8 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int ersteZahl = s.nextInt();
+        int zweiteZahl = s.nextInt();
+
+        System.out.println("Summe: " + (ersteZahl + zweiteZahl));
+    }
+}
