8699ef4289c4d095761b1c4eb113e4b92c65ba30
Pischi04-git <github@fabianhengesbach.de>
2026-01-23 09:14:38 +0100

    aihf

--- 23.01.2026/aufgabe9.java
+++ 23.01.2026/aufgabe9.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class aufgabe9 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        boolean input1 = s.nextBoolean();
-        boolean input2 = s.nextBoolean();
+        boolean a = s.nextBoolean();
+        boolean b = s.nextBoolean();
 
-        System.out.println("------------------------");
+        System.out.println("-----------------------");
 
-        System.out.println(!(input1 & input2));
-        System.out.println((!input1) | (!input2));
+        System.out.println(!(a & b));
+        System.out.println((!a) | (!b));
 
     }
 }
