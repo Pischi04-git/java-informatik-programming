3d313371022f3158c143b14cae32d12dc5fe5e62
Pischi04-git <github@fabianhengesbach.de>
2026-01-30 08:26:14 +0100

    renamed a to zahl

--- 30.1.2026/aufgabe10.java
+++ 30.1.2026/aufgabe10.java
@@ -9,9 +9,9 @@ import java.util.Scanner;
 public class aufgabe10 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int a = s.nextInt();
+        int zahl = s.nextInt();
 
-        if (a % 2 == 0) {
+        if (zahl % 2 == 0) {
             System.out.println("Die Zahl ist gerade.");
         } else {
             System.out.println("Die Zahl ist ungerade.");
