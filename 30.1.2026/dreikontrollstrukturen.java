f461aa8be892e70e92d5436275560b65cc918a96
Pischi04-git <github@fabianhengesbach.de>
2026-01-30 07:59:35 +0100

    Add initial implementation of dreikontrollstrukturen class with conditional logic and added folder for todays lesson

---
+++ 30.1.2026/dreikontrollstrukturen.java
@@ -0,0 +1,12 @@
+public class dreikontrollstrukturen {
+    public static void main(String[] args) {
+        int zahl = 5;
+        if (zahl > 7) {
+            System.out.println("Die Zahl ist größer als 7");
+        } else if (zahl == 7) {
+            System.out.println("Die Zahl ist gleich 7");
+        } else {
+            System.out.println("Die Zahl ist kleiner als 7");
+        }
+    }
+}
