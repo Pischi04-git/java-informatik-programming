f1a43bf079c93ea5024b9437645a87858428eeab
Pischi04-git <github@fabianhengesbach.de>
2026-02-13 08:13:19 +0100

    Refactor output statements in vierZeichenketten class for clarity

--- 13.02.2026/vierZeichenketten.java
+++ 13.02.2026/vierZeichenketten.java
@@ -4,8 +4,8 @@ public class vierZeichenketten {
         String nachname = "Müller";
         
         System.out.println(vorname); // Gibt den Vornamen aus
-        System.out.println(nachname); // Gibt den Nachnamen aus
-        System.out.println(vorname + " " + nachname); // Gibt den vollständigen Namen aus
+        System.out.println(vorname+nachname); // Gibt den vollen Namen ohne Leerzeichen aus
+        System.out.println(vorname + " " + nachname); // Gibt den vollen Namen aus mit Leerzeichen aus
         System.out.println(vorname.length()); // Gibt die Länge des Vornamens aus / Anzahl an Zeichen
         System.out.println(vorname.charAt(2)); // Gibt das Zeichen an der Position 2 des Vornamens aus
         System.out.println(nachname.substring(1, 3)); // Gibt die Zeichen von Position 1 bis 2 des Nachnamens aus
