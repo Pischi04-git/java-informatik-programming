e61592eb05b4568e528d2994a5922342166484a2
Pischi04-git <github@fabianhengesbach.de>
2026-02-13 08:25:02 +0100

    Add aufgabe12 class with main method for string comparison

---
+++ 13.02.2026/aufgabe12.java
@@ -0,0 +1,12 @@
+public class aufgabe12 {
+    public static void main(String[] args) {
+        String name1 = "Fritz"; // Erstellen einer Zeichenkette mit dem Namen "Fritz"
+        String name2 = "Klaus"; // Erstellen einer Zeichenkette mit dem Namen "Klaus"
+        
+        if(name1.equals(name2)) { // Vergleicht die beiden Zeichenketten auf Gleichheit
+            System.out.println("Die Namen sind gleich."); // Gibt aus, dass die Namen gleich sind
+        } else {
+            System.out.println("Die Namen sind verschieden."); // Gibt aus, dass die Namen verschieden
+        }
+    }
+}
