7bbe919a60b78f7798a06d687fcd2fb139b78c62
Pischi04-git <github@fabianhengesbach.de>
2026-03-06 08:49:10 +0100

    Fix output comment in aufgabe16 class for clarity

--- 06.03.2026/aufgabe16.java
+++ 06.03.2026/aufgabe16.java
@@ -7,6 +7,6 @@ public class aufgabe16 {
         zahlen[1][0] = 7; // Weist der Stelle [1][0] den Wert 7 zu
         zahlen[1][1] = 11; // Weist der Stelle [1][1] den Wert 11 zu
         zahlen[1][2] = 13; // Weist der Stelle [1][2] den Wert 13 zu
-        System.out.println(zahlen[0][2]); // Gibt den Wert an der Stelle [0][2]
+        System.out.println(zahlen[0][2]); // Gibt den Wert an der Stelle [0][2] aus
     }    
 }
