1487ad6066c5d88b1dca480b9ed0eaff037abb18
Pischi04-git <github@fabianhengesbach.de>
2026-03-06 08:24:46 +0100

    Add aufgabe15 class to demonstrate array initialization and length output

---
+++ 06.03.2026/aufgabe15.java
@@ -0,0 +1,10 @@
+public class aufgabe15 {
+    public static void main(String[] args) {
+        int[] zahlen = new int[3]; // Deklariert ein Array von Ganzzahlen mit einer Länge von 3
+        zahlen[0] = 2; // Weist der Stelle 0 den Wert 2 zu
+        zahlen[1] = 3; // Weist der Stelle 1 den Wert 3 zu
+        zahlen[2] = 5; // Weist der Stelle 2 den Wert 5 zu
+        System.out.println(zahlen[0]); // Gibt den Wert an der Stelle 0 aus also 2
+        System.out.println(zahlen.length); // Länge des Arrays
+    }
+}
