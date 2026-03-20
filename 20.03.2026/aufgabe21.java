8ec8383b5634f1da4cc07004272a66c8cc39003b
Pischi04-git <github@fabianhengesbach.de>
2026-03-20 08:46:22 +0100

    Add aufgabe21 class to demonstrate ArrayList usage for random number generation and summation

---
+++ 20.03.2026/aufgabe21.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class aufgabe21 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        ArrayList<Integer> liste = new ArrayList<Integer>();
+
+        System.out.println("Gebe die Länge der Liste an: ");
+        int arrayLength = s.nextInt();
+
+        for (int i = 0; i < arrayLength; i++) {
+            liste.add((int) (Math.random() * 100)); // 0 - 99
+        }
+
+        int sum = 0;
+        for (int i = 0; i < liste.size(); i++) {
+            sum += liste.get(i);
+        }
+
+        System.out.println("Die Summe der Werte in der Liste ist: " + sum);
+        s.close();
+    }
+    
+}
