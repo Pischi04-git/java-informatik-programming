2377dca9236a2d3954e5676870714a5c93a01dc8
Pischi04-git <github@fabianhengesbach.de>
2026-03-20 07:50:42 +0100

    Add aufgabe18 class to demonstrate basic ArrayList usage and element retrieval

---
+++ 20.03.2026/aufgabe18.java
@@ -0,0 +1,9 @@
+public class aufgabe18 {
+    public static void main(String[] args) {
+        ArrayList<Integer> liste = new ArrayList<>(); // Deklariert eine ArrayList
+        liste.add(5); // Fügt den Wert 5 zur Liste hinzu
+        liste.add(6); // Fügt den Wert 6 zur Liste hinzu
+        System.out.println(liste.get(0)); // Gibt den Wert an der Stelle 0 aus
+        System.out.println(liste.get(1)); // Gibt den Wert an der Stelle 1 aus
+    }
+}
