2eacec43a482a98b36e603a414ad2efe3afece84
Pischi04-git <github@fabianhengesbach.de>
2026-04-17 08:01:05 +0200

    Remove outdated Java programs and add new implementations for basic programming tasks including arithmetic operations, variable manipulation, array handling, control structures, and string operations.

--- 2026.04.17/aufgabe22.java
+++ 2026.04.17/aufgabe22.java
@@ -8,7 +8,7 @@ public class aufgabe22 {
     private static void summe(int n) {
         int ergebnis = 0;
         for (int zaehler = 1; zaehler <= n; zaehler++) {
-            ergebnis += zaehler;
+            ergebnis += zaehler*ergebnis;
         }
         System.out.println("Die Summe der ersten " + n + " natürlichen Zahlen ist: " + ergebnis);
     }
